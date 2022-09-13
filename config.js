aeb9950367337fcb8373401a52677e4c901c3c81
Valeri2022 <113456461+Valeri2022@users.noreply.github.com>
2022-09-13 23:05:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
+  ['6281311152942'],
+  ['6281311152942'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
